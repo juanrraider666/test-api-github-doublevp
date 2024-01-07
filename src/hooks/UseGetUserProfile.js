9df0e9ca98733ad915ea1b63a69d127d54c907a4
juan.diaz <jpdiaz663@hotmail.com>
2024-01-07 14:49:23 -0500

    fix - error en lectura de variable mal definida

--- src/hooks/UseGetUserProfile.js
+++ src/hooks/UseGetUserProfile.js
@@ -8,6 +8,6 @@ import { useQuery } from 'react-query';
  * @returns {{user: *}}
  */
 export const getOne = (name) => {
-    const { data: user } = useQuery(['user', name], () => getDetail(name), { suspense: true })
-    return {user}
+    const { data: userDetail } = useQuery(['user', name], () => getDetail(name), { suspense: true })
+    return {userDetail}
 }
